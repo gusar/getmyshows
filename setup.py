f2bc6aafcd06be0f8937aa9a0c0f86aaf6e65c89
Andy Lahs <andy.lahs@gmail.com>
2017-06-24 20:24:17 +0100

    Add initial skeleton

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='getmyshows',
+      version='0.0.1',
+      description='GetMyShows',
+      url='https://github.com/gusar/getmyshows',
+      author='Andy Lahs, Ming Zhao',
+      author_email='andy.lahs@gmail.com',
+      license='MIT',
+      packages=['getmyshows'],
+      zip_safe=False)
